e3cb7b085126a0eb64de896edf2a79da73c03671
Picfitweb <129907992+Picfitweb@users.noreply.github.com>
2023-04-04 19:48:33 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-// components
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Main from './components/Main/Main';
-
-// styles
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
